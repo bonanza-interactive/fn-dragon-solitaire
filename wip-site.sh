151784090dc2003f36ed0881795bde306f1b80f3
ashutosh <ashutosh.mishra@bonanzainteractive.com>
2026-04-01 13:23:56 +0530

    Removed all the files of previous template game villit-kakkoset-gaas.

--- wip-site.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-mkdir wip-site-server-artifacts
-cp server/runtime/nemo.jar wip-site-server-artifacts/nemo-server.jar
-cp server/runtime/maya.jar wip-site-server-artifacts/gameMaya.jar
-cp server/runtime/backend.jar wip-site-server-artifacts/gameBackend.jar
-#cp server/runtime/backendPP.jar wip-site-server-artifacts/gameBackendPP.jar
-#cp server/runtime/backendPS.jar wip-site-server-artifacts/gameBackendPS.jar
-#cp server/runtime/backendRA.jar wip-site-server-artifacts/gameBackendRA.jar
